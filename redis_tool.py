bbb45f46a1a599d8a45ef182ba4ad64c50e3a5f0
chenshixin <chenshixin@joyy.sg>
2021-08-10 14:47:41 +0800

    支持多个教室id

--- redis_tool.py
+++ redis_tool.py
@@ -13,10 +13,11 @@ args = parser.parse_args()
 redis_cli = '/data/services/redis.4.0.6/redis-cli'
 
 if __name__ == '__main__':
-    for r in args.room:
+    room_list = args.room.split(',')
+    for r in room_list: 
         log.i("set room %s live platfrom to %s"%(r, args.platform))
-        cmd = [redis_cli, '-h', '10.22.133.80', '-p', '19001', 'hmset', 'room-config:%s'%args.room, 'cur_live_platform', args.platform]
-        log.i('cmd=' + cmd)
+        cmd = [redis_cli, '-h', '10.22.133.80', '-p', '19001', 'hmset', 'room-config:%s'%r, 'cur_live_platform', args.platform]
+        log.i('cmd=' + ' '.join(cmd))
         subprocess.call(cmd)
 
 
